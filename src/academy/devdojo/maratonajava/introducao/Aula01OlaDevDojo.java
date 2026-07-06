7d0c3094abf903d844ce8412d344dca88d5d7450
Emerson Pereira Silva <emersonevandro39@gmail.com>
2026-07-06 15:04:00 -0300

    ____________

---
+++ src/academy/devdojo/maratonajava/introducao/Aula01OlaDevDojo.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.introducao;
+
+/**
+ * Isso e um <b>javadoc</b>
+ */
+public class Aula01OlaDevDojo {
+    public static void main(String[] args) {
+        System.out.println("Olá, mundo!");
+    }
+}
---
+++ src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,19 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula02TiposPrimitivos {
+    public static void main(String[] args) {
+        // int, double, float, char, byte, short, long e boolean
+        int idade = 33;
+        long numeroGrande = (int) 10000000000L;
+        double salarioDouble = 2000.0;
+        float salarioFloat = 2500.0F;
+        byte idadeByte = 10;
+        short idadeShort = 32000;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = 'M';
+
+        System.out.println("A idade é " +idade+ " anos");
+        System.out.println(verdadeiro);
+    }
+}
