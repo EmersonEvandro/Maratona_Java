08313edde626ffd5e2c2fff9e53ca26e817b76ea
Emerson Pereira Silva <emersonevandro39@gmail.com>
2026-07-06 15:20:59 -0300

    ____________

--- src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -12,8 +12,11 @@ public class Aula02TiposPrimitivos {
         boolean verdadeiro = true;
         boolean falso = false;
         char caractere = 'M';
+        String frase = "Não tem limite de valores dentro da classe Strings";
+        var nome2 = "Vegeta";//isso e apartir do java 10
 
         System.out.println("A idade é " +idade+ " anos");
         System.out.println(verdadeiro);
+        System.out.println(frase + ", essa e classe");
     }
 }
